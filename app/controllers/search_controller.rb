d342ff56ff4d7bfe2ceb43a044ec10478f149737
Artem Bondarets <tyom.bond@gmail.com>
2020-04-04 12:48:06 +0300

    Implement  search#search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def search
+    if params[:term].nil?
+    @articles = []
+    else
+      @articles = Article.search_everywhere params[:term]
+    end
   end
 end
