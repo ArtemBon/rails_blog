1a98f6f9e6300fabca9d9eb37adc804bcb883266
Artem Bondarets <tyom.bond@gmail.com>
2020-04-07 22:01:27 +0300

    Remove commenter from comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_065945) do
+ActiveRecord::Schema.define(version: 2020_04_07_185757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2020_04_07_065945) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
